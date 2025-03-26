471ea95be1bc65fddcc9de205743553aa4be673d
Alex Ludvig <ludwig@mnwhl.ru>
2025-03-27 01:47:41 +0300

    Настроен адрес карты

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
 document.addEventListener('DOMContentLoaded', function () {
-  // Устанавливаем дату свадьбы
   const weddingDate = new Date('2025-04-25 00:00:00').getTime()
 
   function updateCountdown() {
@@ -22,37 +21,28 @@ document.addEventListener('DOMContentLoaded', function () {
     document.getElementById('seconds').innerText = seconds
   }
 
-  // Обновляем счётчик каждую секунду
   setInterval(updateCountdown, 1000)
   updateCountdown()
 })
 
 document.addEventListener('DOMContentLoaded', () => {
-  // Находим все кнопки табов
   const tabButtons = document.querySelectorAll('.map__tab-button')
-  const tabPanes = document.querySelectorAll('.tab-pane')
+  const mapFrame = document.querySelector('.map__iframe')
 
-  // Добавляем обработчик для каждой кнопки
-  tabButtons.forEach(button => {
+  const mapUrls = {
+    registry_office: 'https://yandex.ru/map-widget/v1/?ll=30.244703%2C59.987105&z=17.62&text=Отдел ЗАГС Приморского района&pt=30.244703,59.987105',
+    banquet: 'https://yandex.ru/map-widget/v1/?ll=30.244631%2C59.993622&z=17.62&text=Академия Вкуса&pt=30.244631,59.993622',
+  }
+
+  tabButtons.forEach((button, index) => {
     button.addEventListener('click', () => {
-      // Убираем активный класс у всех кнопок
-      tabButtons.forEach(btn => {
-        btn.classList.remove('active')
-      })
-
-      // Убираем активный класс у всех панелей
-      tabPanes.forEach(pane => {
-        pane.classList.remove('active')
-      })
-
-      // Добавляем активный класс нажатой кнопке
+      tabButtons.forEach(btn => btn.classList.remove('active'))
       button.classList.add('active')
 
-      // Находим и показываем соответствующую панель
-      const tabId = button.getAttribute('data-tab')
-      const targetPane = document.getElementById(tabId)
-      if (targetPane) {
-        targetPane.classList.add('active')
+      if (index === 0) {
+        mapFrame.src = mapUrls.registry_office
+      } else {
+        mapFrame.src = mapUrls.banquet
       }
     })
   })
